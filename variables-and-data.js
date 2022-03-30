67ddb24eb7009740078cd37a731176397d5eec50
Mirjam Wolf <mwolf14@cnm.edu>
2022-03-30 13:00:43 -0600

    Added practice file variables-and-data.js

--- variables-and-data.js
+++ variables-and-data.js
@@ -16,3 +16,20 @@ console.log(string);
     *Conditionals, Functions, Scope, and Loops
 */
 
+// Equals
+let equals = 1 === 1;
+
+// Greater than
+let greaterThan = 5 > 1;
+
+// Less than
+let lessThan = 2 < 10;
+
+// Greater than Equals
+let greaterThanEq = 5 >= 5;
+
+// Less than Equals
+let lessThanEq = 4 <= 9;
+
+// Not Equals
+let notEquals = 5 !== 2;
