ea829f71729c9404a1b00d372d3d57eea23259a2
Mirjam Wolf <mwolf14@cnm.edu>
2022-03-30 16:29:34 -0600

    Added practice file variables-and-data.js, added coding examples with comments, added functions and if and else statements, added scopes,arrays,index in arrays,added arrays file, while loop

--- arrays.js
+++ arrays.js
@@ -4,3 +4,17 @@ for(let i = 0; i<ourArray.length; i++) {
     console.log(ourArray[i]);
 }
 
+let arrLen = ourArray.length;
+for(let i = 0; i<arrLen; i++) { // value is already calculated here, so saves time compared to above
+    console.log(ourArray[i]);
+}
+// Try to avoid nested loops as they can take up too much time!
+
+// While Loops: run until a conditional is false; while(true) would run forever
+let x = 0
+while (x < 10) {
+    console.log('Ran');
+    x = x + 1; // Incrementor inside while loop, 'Ran' will be printed 10 times.
+}
+
+// Be careful with while loops as they cun crash your code (they are infinite)
