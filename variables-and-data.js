1a1eff8b9e30131b7fc4d946ed316397f900b810
Mirjam Wolf <mwolf14@cnm.edu>
2022-03-30 12:31:15 -0600

    Added practice file variables-and-data.js

---
+++ variables-and-data.js
@@ -0,0 +1,14 @@
+/*
+    *Variables, Data Types, and Typing
+ */
+
+let add = 1+2;
+let sub = 2-1;
+let mult = 2*4;
+let division = 4/2;
+let mod = 5%2;
+
+let string = "String 1" + "String 2";
+
+console.log(string);
+
